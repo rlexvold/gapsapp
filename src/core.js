ca6f5bebc34c9905685a85af6a48f2750c04b5b2
Randy Lexvold <rlexvold@gmail.com>
2016-11-27 19:53:59 -0800

    Updates to preferences

--- src/core.js
+++ src/core.js
@@ -1,5 +1,5 @@
-import {List} from 'immutable'
+import {List} from 'immutable';
 
 export function setEntries(state, entries) {
-    return state.set('entries', List(entries))
+    return state.set('entries', List(entries));
 }
